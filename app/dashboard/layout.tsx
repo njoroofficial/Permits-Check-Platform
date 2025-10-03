d3e6062d21914d12ecff155cf472a8dbec7bd4b8
njoroofficial <mosesn579@gmail.com>
2025-10-03 09:07:47 +0300

    Included next.js dynamicIO tp hanlde dynamic data fetching

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,9 +1,10 @@
 import type React from "react";
+import { Suspense } from "react";
 
 export default function DashboardLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return children;
+  return <Suspense fallback={<div>Loading...</div>}>{children}</Suspense>;
 }
