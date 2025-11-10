04d1301f733c59ae87daa797ab6586e5467da741
njoroofficial <mosesn579@gmail.com>
2025-11-10 11:34:54 +0300

    Implement posthog

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,19 @@ const nextConfig: NextConfig = {
   experimental: {
     turbopackFileSystemCacheForDev: true,
   },
+  async rewrites() {
+    return [
+      {
+        source: "/ingest/static/:path*",
+        destination: "https://us-assets.i.posthog.com/static/:path*",
+      },
+      {
+        source: "/ingest/:path*",
+        destination: "https://us.i.posthog.com/:path*",
+      },
+    ];
+  },
+  skipTrailingSlashRedirect: true,
 };
 
 export default nextConfig;
